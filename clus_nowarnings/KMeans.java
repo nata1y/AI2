3092275bb6aab9cbff3e865c2f51d737d20b5224
nata1y <oxi-flent@mail.ru>
2018-10-19 12:58:34 +0200

    blaaa

--- clus_nowarnings/KMeans.java
+++ clus_nowarnings/KMeans.java
@@ -54,22 +54,76 @@ public class KMeans extends ClusteringAlgorithm
 			clusters[ic] = new Cluster();
 	}
 
-	public euclidian_distance(Cluster cluster){
-		for(int i = 0; i < trainData.size(); i++){
-			double[] means = new double[cluster.currentMembers.size()];
-			for(int s = 0; s < k; s++){
-				double currentMean = 0;
-				for(int j = 0; j < dim; j++){
-					currentMean += sqrt(Math.pow(trainData.get(i)[j] - clusters[s].prototype[j], 2)); 
-				}
-				means[i] = currentMean;
-			}
+	public Double euclidian_distance(Cluster cluster, float[] dataVec){
+		Double distance = new Double(0);
+		for(int i = 0; i < dataVec.length; i++){
+			distance += Math.pow(dataVec[i] - cluster.prototype[i], 2); 
 		}
-
-
+		return (Double)Math.sqrt(distance);
 	}
 
+	public int findPrototype(float[] dataVec){
+		Double[] dists = new Double[clusters.length];
+		for(int i = 0; i < clusters.length; i++){
+			dists[i] = euclidian_distance(clusters[i], dataVec);
+		}
 
+		List help = Arrays.asList(dists);
+		Double minDist = (Double)Collections.min(help);
+		int idx = help.indexOf(minDist);
+		return idx;
+	}
+
+	/// compute mean for one cluster
+	public float[] computeMean(Cluster clust){
+		float[] mean = new float[dim];
+		for(int i = 0; i < dim; i ++){
+			mean[i] = 0;
+			Iterator iterator = clust.currentMembers.iterator();
+			while(iterator.hasNext()){
+				mean[i] += trainData.get((int)iterator.next())[i];
+			}
+			mean[i] /= clust.currentMembers.size();
+		}
+		return mean;
+	}
+
+	public void oneEpoch(){
+		int[] correspondPrtotypes = new int[trainData.size()];
+
+		for(int i = 0; i < clusters.length; i ++){
+			clusters[i].previousMembers = clusters[i].currentMembers; 
+			clusters[i].currentMembers = new HashSet<Integer>();
+			System.out.println(clusters[i].previousMembers);
+		}
+		/// finding corresponding cluster (prototype) for each data vector
+		/// reassigning datavectors to new clusters step 2
+		for(int i = 0; i < trainData.size(); i++){
+			correspondPrtotypes[i] = findPrototype(trainData.get(i));
+			clusters[correspondPrtotypes[i]].currentMembers.add(i);
+		}
+
+		/// compute new centroid step 3
+		for(int i = 0; i < clusters.length; i++){
+			clusters[i].prototype = computeMean(clusters[i]);
+		}
+	}
+
+	public boolean checkCond(){
+		for(int i = 0; i < clusters.length; i ++){
+				Iterator iterator = clusters[i].currentMembers.iterator();
+				Iterator iterator2 = clusters[i].previousMembers.iterator();
+				while(iterator.hasNext() && iterator2.hasNext()){
+					if (iterator.next() != iterator2.next()){
+						return true;
+					}
+				}
+				if (iterator2.hasNext()){
+					return true;
+				}
+		}
+		return false;
+	}
 	
 	public boolean train()
 	{
@@ -80,19 +134,30 @@ public class KMeans extends ClusteringAlgorithm
 		// Step 4: repeat until clustermembership stabilizes
 
 		///Step 1
-		List<Integer> shuffeledData = new ArrayList<>(trainData);
-		Collections.shuffle(shuffeledData);
-		int clusterCount = -1;
-		for(int i=0; i<shuffeledData.size(); i++) {
-			if(i%(shuffeledData.size()/k)) {
-				clusterCount++;
-			}
-			clusters[clusterCount].currentMembers.add(shuffeledData.get(i));
+		Collections.shuffle(trainData);
+		boolean condition = true;
+
+		clusters = new Cluster[k];
+		for(int i = 0; i < k; i++){
+			clusters[i] = new Cluster();
 		}
 
-		///Step 2
-		for(int i = 0; i < k; i++){
-			clusters[k].prototype = 
+		for(int n = 0; n < k; n++){
+			for(int i=(int)(n*trainData.size()/k); i<(int)((n+1)*trainData.size()/k); i++) {
+				clusters[n].currentMembers.add(i);
+				clusters[n].previousMembers.add(0);
+			}
+		}
+
+		for(int i = 0; i<clusters.length; i++){
+			clusters[i].prototype = computeMean(clusters[i]);
+		}
+
+		while(condition){
+			//System.out.println(condition);
+			oneEpoch();
+			/// step 4
+			condition = checkCond();
 		}
 
 		return false;
